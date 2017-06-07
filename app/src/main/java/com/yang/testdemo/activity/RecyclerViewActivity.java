609b6ef897da8a8ef6955a06560d74abbde54c46
yangle <mr.yang0229@foxmail.com>
2017-06-07 14:41:21 +0800

    EndlessRecyclerOnScrollListener

--- app/src/main/java/com/yang/testdemo/activity/RecyclerViewActivity.java
+++ app/src/main/java/com/yang/testdemo/activity/RecyclerViewActivity.java
@@ -3,13 +3,13 @@ package com.yang.testdemo.activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.yang.testdemo.R;
+import com.yang.testdemo.listener.EndlessRecyclerOnScrollListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,53 +59,23 @@ public class RecyclerViewActivity extends BaseActivity {
             }
         });
 
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            //用来标记是否正在向最后一个滑动
-            boolean isSlidingToLast = false;
-
+        recyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener() {
             @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
-                // 当不滚动时
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    //获取最后一个完全显示的ItemPosition
-                    int lastVisibleItem = manager.findLastCompletelyVisibleItemPosition();
-                    int totalItemCount = manager.getItemCount();
+            public void onLoadMore() {
+                recyclerViewAdapter.setLoadMore(true);
 
-                    // 判断是否滚动到底部，并且是向上滚动
-                    if (lastVisibleItem == (totalItemCount - 1) && isSlidingToLast) {
-                        //加载更多功能的代码
-                        Log.i("上拉加载更多", lastVisibleItem + "");
-                        recyclerViewAdapter.setLoadMore(true);
-
-                        new Timer().schedule(new TimerTask() {
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        initData();
-                                        recyclerViewAdapter.setLoadMore(false);
-                                    }
-                                });
+                                initData();
+                                recyclerViewAdapter.setLoadMore(false);
                             }
-                        }, 1000);
+                        });
                     }
-                }
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                //dx用来判断横向滑动方向，dy用来判断纵向滑动方向
-                if (dy > 0) {
-                    //大于0表示正在向右滚动
-                    isSlidingToLast = true;
-                } else {
-                    //小于等于0表示停止或向左滚动
-                    isSlidingToLast = false;
-                }
+                }, 1000);
             }
         });
     }
--- app/src/main/java/com/yang/testdemo/listener/EndlessRecyclerOnScrollListener.java
+++ app/src/main/java/com/yang/testdemo/listener/EndlessRecyclerOnScrollListener.java
@@ -4,45 +4,47 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 /**
- * Created by yangle on 2017/1/12.
+ * RecyclerView滑动监听
+ * Created by yangle on 2017/6/7.
  */
-public abstract class EndlessRecyclerOnScrollListener extends
-        RecyclerView.OnScrollListener {
+public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
 
-    private int previousTotal = 0;
-    private boolean loading = true;
-    int firstVisibleItem, visibleItemCount, totalItemCount;
+    //用来标记是否正在向最后一个滑动
+    private boolean isSlidingToLast = false;
 
-    private int currentPage = 1;
+    @Override
+    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+        LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        // 当不滚动时
+        if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+            //获取最后一个完全显示的ItemPosition
+            int lastVisibleItem = manager.findLastCompletelyVisibleItemPosition();
+            int totalItemCount = manager.getItemCount();
 
-    private LinearLayoutManager mLinearLayoutManager;
-
-    public EndlessRecyclerOnScrollListener(
-            LinearLayoutManager linearLayoutManager) {
-        this.mLinearLayoutManager = linearLayoutManager;
+            // 判断是否滚动到底部，并且是向上滚动
+            if (lastVisibleItem == (totalItemCount - 1) && isSlidingToLast) {
+                //加载更多功能的代码
+                onLoadMore();
+            }
+        }
     }
 
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
-
-        visibleItemCount = recyclerView.getChildCount();
-        totalItemCount = mLinearLayoutManager.getItemCount();
-        firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
-
-        if (loading) {
-            if (totalItemCount > previousTotal) {
-                loading = false;
-                previousTotal = totalItemCount;
-            }
-        }
-        if (!loading
-                && (totalItemCount - visibleItemCount) <= firstVisibleItem) {
-            currentPage++;
-            onLoadMore(currentPage);
-            loading = true;
+        //dx用来判断横向滑动方向，dy用来判断纵向滑动方向
+        if (dy > 0) {
+            //大于0表示正在向上滚动
+            isSlidingToLast = true;
+        } else {
+            //小于等于0表示停止或向下滚动
+            isSlidingToLast = false;
         }
     }
 
-    public abstract void onLoadMore(int currentPage);
+    /**
+     * 加载更多回调
+     */
+    public abstract void onLoadMore();
 }
