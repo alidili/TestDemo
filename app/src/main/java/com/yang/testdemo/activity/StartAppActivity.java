9a013e36713088d6ad817837aebd6700ddd8aa35
yangle <mr.yang0229@foxmail.com>
2017-06-13 17:56:10 +0800

    获取第三方应用图标

--- app/src/main/java/com/yang/testdemo/activity/StartAppActivity.java
+++ app/src/main/java/com/yang/testdemo/activity/StartAppActivity.java
@@ -2,11 +2,16 @@ package com.yang.testdemo.activity;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.yang.testdemo.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -17,6 +22,9 @@ import butterknife.OnClick;
 
 public class StartAppActivity extends BaseActivity {
 
+    @Bind(R.id.iv_icon)
+    ImageView ivIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +32,7 @@ public class StartAppActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.btn_method_one, R.id.btn_btn_method_two, R.id.btn_btn_method_three})
+    @OnClick({R.id.btn_method_one, R.id.btn_btn_method_two, R.id.btn_btn_method_three, R.id.btn_get_icon})
     public void onViewClicked(View view) {
         Intent intent = null;
 
@@ -45,12 +53,27 @@ public class StartAppActivity extends BaseActivity {
                 intent.setAction("android.intent.action.EndlessLinearLayoutActivity");
                 break;
 
+            case R.id.btn_get_icon:
+                ivIcon.setBackground(getAppIcon("com.lzx.demo"));
+                break;
+
             default:
                 break;
         }
 
         if (intent != null) {
             startActivity(intent);
+        }
+    }
+
+    public Drawable getAppIcon(String packName) {
+        try {
+            PackageManager packageManager = getPackageManager();
+            ApplicationInfo info = packageManager.getApplicationInfo(packName, 0);
+            return info.loadIcon(packageManager);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
